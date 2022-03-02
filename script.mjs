52258574a2d3511a1c03bfab2c182e21d20c2fc1
jacklarnaque <grousset1065@live.fr>
2022-03-02 13:38:14 +0000

    init

---
+++ script.mjs
@@ -0,0 +1,11 @@
+
+
+// import multiply from "./module.js"
+
+// console.log(multiply(1, 5))
+const buttonHelp = document.getElementById('buttonHelp')
+
+buttonHelp.addEventListener('click', () => {
+    location.assign('https://www.youtube.com/watch?v=FRSIXQv7Y7g')
+})
+
